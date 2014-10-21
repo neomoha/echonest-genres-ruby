a913ac0caf38031d1d9310bda9719c9e1c16e23b
Mohamed Sordo <mohamed.sordo@gmail.com>
2014-10-21 17:40:17 +0200

    added source code

---
+++ README.md
@@ -0,0 +1,49 @@
+Get artist genre(s) from The Echo Nest API
+======
+
+A small script in Ruby to get the artist genre (or a list of genres) from the Echo Nest API, if the artist exists
+
+Dependencies
+------
+
+* [echowrap](https://github.com/timcase/echowrap)
+* [httparty](https://github.com/jnunemaker/httparty)
+* [json](https://rubygems.org/gems/json)
+
+How to use this code
+------
+
+Edit the script [echonest_genre.rb](https://github.com/neomoha/echonest-genres-ruby/blob/master/echonest_genre.rb) and change the line:
+
+API_KEY = 'YOUR_ECHONEST_API_KEY_HERE'
+
+with your own Echo Nest API key. For more information about how to get the API key, please refer to Echo Nest [API Documentation](https://developer.echonest.com/account/register)
+
+Examples
+------
+
+$ ruby echonest_genre.rb -h
+Usage: echonest_genre.rb [options]
+    -n, --name NAME                  Artist name
+    -m, --genre_method METHOD        Get "single" genre or a "list" genres (default="single")
+    
+$ ruby echonest_genre.rb -n Diversidad
+Artist 'Diversidad' has genre 'european' in echonest
+
+$ ruby echonest_genre.rb -n Diversidad -m list
+Artist Diversidad does not have genre information in echonest
+
+$ ruby echonest_genre.rb -n Luche -m list
+Artist Luche does not have genre information in echonest
+
+$ ruby echonest_genre.rb -n Luche -m single
+Artist 'Luche' has genre 'italian rap' in echonest
+
+$ ruby echonest_genre.rb -n Weezer -m single
+Artist 'Weezer' has genre 'rock' in echonest
+
+$ ruby echonest_genre.rb -n Weezer -m list
+Artist Weezer has genres ["permanent wave", "alternative rock", "punk christmas", "power pop", "indie christmas", "pop rock", "pop christmas", "rock"] in echonest
+
+$ ruby echonest_genre.rb -n Weezer
+Artist 'Weezer' has genre 'rock' in echonest
---
+++ echonest_genre.rb
@@ -0,0 +1,71 @@
+#!/usr/bin/env ruby
+
+require 'echowrap'
+require 'optparse'
+require 'httparty'
+require 'json'
+
+API_KEY = 'YOUR_ECHONEST_API_KEY_HERE'
+
+Echowrap.configure do |config|
+  config.api_key = API_KEY
+end
+
+def find_artist(artist_name)
+  artist = Echowrap.artist_search(:name => artist_name, :results => 1, :bucket => ['genre'])
+end
+
+def get_genres(artist)
+  genres = Array.new()
+  for genre in artist.attrs[:genres]
+    genres.push(genre[:name])
+  end
+  return genres
+end
+
+def get_genre(artist)
+  genre = nil
+  url = "http://developer.echonest.com/api/v4/artist/genres?api_key="<<API_KEY<<"&id="<<artist.id
+  response = HTTParty.get(url)
+  parsed_response = JSON.parse(response.body)
+  if parsed_response["response"]["status"]["code"] == 0
+    if parsed_response["response"]["terms"]["genre"].empty? == false
+      genre = parsed_response["response"]["terms"]["genre"]
+    end
+  end
+end
+
+if __FILE__ == $0
+  options = OpenStruct.new
+  options.genre_method="single"
+  OptionParser.new do |opts|
+    opts.banner = "Usage: echonest_genre.rb [options]"
+    opts.on('-n', '--name NAME', 'Artist name') { |v| options[:name] = v }
+    opts.on('-m', '--genre_method METHOD', 'Get "single" genre or a "list" genres (default="single")') { |v| options[:genre_method] = v }
+  end.parse!
+  
+  if options.name == nil
+    abort("ERROR: You should specify an artist name")
+  end
+  
+  result = find_artist(options.name)
+  if result.count > 0
+    if options.genre_method == "list"
+      genres = get_genres(result[0])
+      if genres.count > 0
+        puts "Artist #{result[0].name} has genres #{genres} in echonest"
+      else
+        puts "Artist #{result[0].name} does not have genre information in echonest"
+      end
+    else # single
+      genre = get_genre(result[0])
+      if genre.empty? == false
+        puts "Artist '#{result[0].name}' has genre '#{genre}' in echonest"
+      else
+        puts "Artist '#{result[0].name}' does not have genre information in echonest"
+      end
+    end
+  else
+    puts "Artist #{options[:name]} does not exist in echonest"
+  end
+end
